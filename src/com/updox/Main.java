bda10674fb6c111295a34b90301b4a7605408856
akaidc2 <alex.kaidan@wendys.com>
2017-05-21 16:45:40 -0400

    still getting git bearings within intellij

---
+++ src/com/updox/Main.java
@@ -0,0 +1,8 @@
+package com.updox;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/updox/PrimeNumberGenerator.java
@@ -0,0 +1,11 @@
+package com.updox;
+import java.util.List;
+
+/**
+ * Created by akaid01 on 5/21/17.
+ */
+public interface PrimeNumberGenerator {
+    List<Integer> generate (int startingValue, int endingValue );
+
+
+}
